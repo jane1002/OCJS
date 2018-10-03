7dfdf253e6f868f3668f49683a5bf99c835615be
Yang Fan <YangFAN@Yangs-MacBook-Pro.local>
2018-10-03 01:59:00 -0500

    question.pdf change.

--- src/Test.java
+++ src/Test.java
@@ -7,9 +7,29 @@ public class Test {
 		// Q2: String immutable
 		String product = new String("Pen");
 		product.toLowerCase();
-		System.out.println(product);
+		//System.out.println(product);
 		String a = product.concat(" BOX".toLowerCase());
-		System.out.println(a);
+		//System.out.println(a);
 		//System.out.print(product.substring(4, 6));
+
+		/*
+		Q.26
+		 */
+		int[] arr = {1, 2, 3};
+
+		for(int var: arr) {
+			int i = 1;
+
+			while(i<=var) {
+				System.out.println(i++);
+			}
+		}
+
+		/*
+		Q. 39
+		 */
+		char m;
+		boolean am;
+		System.out.println(m);
 	}
 }
